6ddf362b56697fe3ab8905bd727f59d0a8917056
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-10-14 07:34:11 +0530

    removed unnecessary pages |  added required ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
+import ListPage from "./pages/list";
+
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import HierarchyPage from "@/pages/hierarchy";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
+      <Route element={<ListPage />} path="/list" />
+      <Route element={<HierarchyPage />} path="/hierarchy" />
     </Routes>
   );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -9,20 +9,12 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "List",
+      href: "/list",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
+      label: "Hierarchy",
+      href: "/hierarchy",
     },
   ],
   navMenuItems: [
@@ -31,27 +23,12 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "List",
+      href: "/list",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
+      label: "Hierarchy",
+      href: "/hierarchy",
     },
   ],
-  links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
