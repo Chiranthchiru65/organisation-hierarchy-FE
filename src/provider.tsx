ecf56b9c9fd0389d05d30877a3c8ee0ed85ff615
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-11-04 20:25:08 +0530

    default theme set to light

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,6 +1,29 @@
+// import type { NavigateOptions } from "react-router-dom";
+
+// import { HeroUIProvider } from "@heroui/system";
+// import { useHref, useNavigate } from "react-router-dom";
+
+// declare module "@react-types/shared" {
+//   interface RouterConfig {
+//     routerOptions: NavigateOptions;
+//   }
+// }
+
+// export function Provider({ children }: { children: React.ReactNode }) {
+//   const navigate = useNavigate();
+
+//   return (
+//     <HeroUIProvider navigate={navigate} useHref={useHref}>
+//       {children}
+//     </HeroUIProvider>
+//   );
+// }
+
 import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
+import { useTheme, ThemeProps } from "@heroui/use-theme";
+import { useEffect } from "react";
 import { useHref, useNavigate } from "react-router-dom";
 
 declare module "@react-types/shared" {
@@ -14,7 +37,21 @@ export function Provider({ children }: { children: React.ReactNode }) {
 
   return (
     <HeroUIProvider navigate={navigate} useHref={useHref}>
+      <ThemeInitializer />
       {children}
     </HeroUIProvider>
   );
 }
+
+const ThemeInitializer = () => {
+  const { setTheme } = useTheme(ThemeProps.LIGHT);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem(ThemeProps.KEY);
+    if (!storedTheme) {
+      setTheme(ThemeProps.LIGHT);
+    }
+  }, [setTheme]);
+
+  return null;
+};
